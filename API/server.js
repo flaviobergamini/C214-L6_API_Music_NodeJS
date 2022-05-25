2ff15e14f605979c3f09c9ddbd04cd09f44e27c6
= <=>
2022-05-25 19:32:37 -0300

    Removendo pasta com o projeto da API e trazendo conteúdo para a raiz do repositório

--- API/server.js
+++
@@ -1,9 +0,0 @@
-require('./src/infrastructure/database');
-const app = require('./src/infrastructure/rest_server');
-const port = 6000
-
-app.listen(port, () => {
-    console.log('Use http://localhost:' + port);
-    console.log('REST server running on port ' + port + '...');
-
-});
